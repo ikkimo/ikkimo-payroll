91a83cd31253cf962173962ad803ce2b661e1314
Luana Defourny <ikkimo@ikkimo.com>
2026-02-17 09:40:38 +0000

    fix: moved use client to top of app/app/page.tsx

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { JSX, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
